67ae3897b7ec393db3c793bd2396061b111f4ec2
Daniel Palmer <daniel@0x0f.com>
2020-12-29 14:37:58 +0900

    mtd: spinand: add support for Foresee FS35ND0*G parts
    Add support for the various Foresee FS35ND0*G parts manufactured by Longsys.
    
    Signed-off-by: Daniel Palmer <daniel@0x0f.com>
    
    Link: https://datasheet.lcsc.com/szlcsc/2008121142_FORESEE-FS35ND01G-S1Y2QWFI000_C719495.pdf

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -941,6 +941,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&ato_spinand_manufacturer,
 	&esmt_c8_spinand_manufacturer,
 	&gigadevice_spinand_manufacturer,
+	&longsys_spinand_manufacturer,
 	&macronix_spinand_manufacturer,
 	&micron_spinand_manufacturer,
 	&paragon_spinand_manufacturer,
